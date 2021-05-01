e8a86e3e0a5fd32bca9116224415717866cb67ed
Anton Cherepkov <anthonycherepkov@gmail.com>
2021-05-01 21:22:42 +0300

    update example

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import matplotlib.pyplot as plt
 
 
 if __name__ == "__main__":
-    N = 500
+    N = 50
     D = 10
 
     cluster1_center = np.ones(D) * 5
@@ -15,7 +15,7 @@ if __name__ == "__main__":
     X = np.concatenate([
         np.random.rand(N, D) + cluster1_center,
         np.random.rand(N, D) + cluster2_center,
-        np.random.rand(N, D) + cluster2_center
+        np.random.rand(N, D) + cluster3_center
     ], axis=0)
     X_transformed = TSNE(n_iter=1000).fit_transform(X)
 
